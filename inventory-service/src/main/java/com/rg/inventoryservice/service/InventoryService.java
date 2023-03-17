37ef0deb92de95e79ca732b21199f75549375cdb
Rakhman Gaibatov <gaibatov.ra@yandex.ru>
2023-03-17 17:20:58 +0300

    order and inventory service communication add inventory check before placing order

--- inventory-service/src/main/java/com/rg/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/rg/inventoryservice/service/InventoryService.java
@@ -1,9 +1,12 @@
 package com.rg.inventoryservice.service;
 
+import com.rg.inventoryservice.dto.InventoryResponse;
 import com.rg.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -13,7 +16,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .isInstock(inventory.getQuantity() > 0)
+                                .skuCode(inventory.getSkuCode())
+                                .build()
+                ).toList();
+
     }
 }
--- order-service/src/main/java/com/rg/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/rg/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.rg.orderservice.service;
 
+import com.rg.orderservice.dto.InventoryResponse;
 import com.rg.orderservice.dto.OrderLineItemsDto;
 import com.rg.orderservice.dto.OrderRequest;
 import com.rg.orderservice.model.Order;
@@ -7,8 +8,10 @@ import com.rg.orderservice.model.OrderLineItems;
 import com.rg.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -25,15 +29,31 @@ public class OrderService {
 
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
-                .map(this::mapToDto)
+                .map(this::mapFromDto)
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = orderLineItems.stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        InventoryResponse[] inventoryResponses = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInstock);
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
-    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+    private OrderLineItems mapFromDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
